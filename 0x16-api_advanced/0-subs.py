cdc1bdae041a8e9bcfd45430963d0708f3545c24
JAPHETH <japho0740@gmail.com>
2024-01-17 12:52:06 -0800

    Add solution to Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,6 @@ number of subscribers for a given subreddit"""
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API and returns the number of
     subscribers for a given subreddit"""
